bf73002973b91625e36e6b266485cb974c0ccc0a
jewelmu24-meet <139688840+jewelmu24-meet@users.noreply.github.com>
2023-07-20 18:14:01 +0300

    python

--- main.py
+++ main.py
@@ -1,4 +1,10 @@
-def create_youtube_video():
-def dislike(): 
-def like()
+def create_youtube_video(title, description):
+    utubevid= {'title': title, 'description': description, 'likes':0, 'dislikes':0, "comments":{}}
+    return utubevid
+
+
+def like(utubevid):
+	if 'likes' in utubevid:
+        utubevid[]
+
 def add_comment():
